b9d329b6764397f76e05655466e10b293af2c41d
Alex Charland <ac3charland@gmail.com>
2018-09-25 12:12:57 -0500

    Fixed typo in Ganon image URL

--- logic.js
+++ logic.js
@@ -51,7 +51,7 @@ function Character(name, health, attPwr, counterPwr, src, value) {
 
 // Create our four characters
 var Link = new Character("Link", 100, 40, 50, "assets/images/link.png", "0");
-var Ganon = new Character("Ganon", 200, 10, 55, "assets/images/Ganon.jpg", "1");
+var Ganon = new Character("Ganon", 200, 10, 55, "assets/images/ganon.jpg", "1");
 var Yiga = new Character("Yiga", 85, 60, 10, "assets/images/yiga.jpg", "2");
 var Lynel = new Character("Lynel", 150, 30, 45, "assets/images/lynel.png", "3");
 
