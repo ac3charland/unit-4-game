209e260f6507c7bb39adab21ec9090ed4fd9857c
Alex Charland <ac3charland@gmail.com>
2018-09-24 21:49:14 -0500

    Code working and commented, just need to balance characters.

--- logic.js
+++ logic.js
@@ -1,27 +1,46 @@
+// Create variables to track the various states of the game
 var gameStarted = false;
 var enemySelected = false;
 var gameOver = false;
 var gameWon = false;
+
+// Create placeholder variables for the character the user picks to play as...
 var userCharacter;
+// The other characters, which become enemies...
 var enemies = [];
+// And the currently selected enemy.
 var currentDefender;
 
+// Initialize the game's audio files
 var battleSong = new Audio("assets/sounds/battle.mp3");
 battleSong.loop = true;
 var gameWonSong = new Audio("assets/sounds/gameWon.mp3");
 var gameOverSong = new Audio("assets/sounds/gameOver.mp3");
 
+// Create a Character prototype which all characters inherit from
 function Character(name, health, attPwr, counterPwr, src, value) {
     this.name = name;
     this.health = health;
+
+    // Attack power
     this.attPwr = attPwr;
+    // Current attack power
     this.currentAttPwr = attPwr;
+    // Counter attack power
     this.counterPwr = counterPwr;
+
+    // Url for character's picture
     this.src = src;
+
+    // Unique numerical value given to each character
     this.value = value;
+
+    // Increments the character's current attack power.
     this.attIncrement = function () {
         this.currentAttPwr += this.attPwr;
     }
+
+    // Generates the HTML for the character's tile.
     this.createHTML = function () {
         var charDiv = $("<div id=" + this.name + " class='character-tile text-center active'>")
         charDiv.attr("value", this.value);
@@ -30,15 +49,16 @@ function Character(name, health, attPwr, counterPwr, src, value) {
     }
 }
 
+// Create our four characters
 var Link = new Character("Link", 100, 50, 50, "assets/images/link.png", "0");
 var Ganon = new Character("Ganon", 200, 10, 55, "assets/images/Ganon.jpg", "1");
 var Yiga = new Character("Yiga", 85, 80, 10, "assets/images/yiga.jpg", "2");
 var Lynel = new Character("Lynel", 150, 30, 25, "assets/images/lynel.png", "3");
 
+// Put our four characters in an array, where each character's index matches their .value property.
 var characters = [Link, Ganon, Yiga, Lynel];
 
-
-
+// Creates a Bootstrap column for a character, given a character object and a specified Bootstrap column size
 function createCharacterColumn(character, size) {
     var characterColumn = $('<div>');
     characterColumn.addClass("col-md-" + size);
@@ -46,6 +66,7 @@ function createCharacterColumn(character, size) {
     return characterColumn;
 }
 
+// Populates the character selection row with all characters.
 function moveAllToSelect() {
     for (var index in characters) {
         var character = characters[index];
@@ -54,6 +75,7 @@ function moveAllToSelect() {
     }
 }
 
+// Creates a character in the #userRow row, based on the character index passed to the function
 function moveToUser(index) {
     $("#userRow").empty();
     var characterSelected = characters[index];
@@ -61,6 +83,7 @@ function moveToUser(index) {
     $("#userRow").append(characterColumn);
 }
 
+// Populates the #enemyRow row with the remaining enemies
 function populateEnemyPool() {
     $("#enemyRow").empty();
     for (var index in enemies) {
@@ -70,6 +93,7 @@ function populateEnemyPool() {
     }
 }
 
+// Removes a character of a given value from the enemies array
 function removeFromEnemyPool(value) {
     for (var index in enemies) {
         enemyValue = enemies[index].value;
@@ -79,6 +103,7 @@ function removeFromEnemyPool(value) {
     }
 }
 
+// Populates the #defenderRow row with a character tile based on a character value
 function moveToDefense(index) {
     $("#defenderRow").empty();
     var characterSelected = characters[index];
@@ -87,20 +112,34 @@ function moveToDefense(index) {
     $("#defenderRow").append(characterColumn);
 }
 
+// Logic for each attack
 function attack() {
-    $("#userStatus").text(userCharacter.name + " attacked " + currentDefender.name + " for " + userCharacter.currentAttPwr + " damage.");
+    // The player's character performs an attack
     currentDefender.health -= userCharacter.currentAttPwr;
+    // Write the player's attack to the DOM
+    $("#userStatus").text(userCharacter.name + " attacked " + currentDefender.name + " for " + userCharacter.currentAttPwr + " damage.");
+    // The character's attack power goes up
+    userCharacter.currentAttPwr += userCharacter.attPwr;
+    // If the current enemy is dead...
     if (currentDefender.health <= 0) {
+        // Clear the #defenderRow
         $("#defenderRow").empty();
+        // Write that the enemy was defeated to the DOM
         $("#defenderStatus").text(currentDefender.name + " was defeated!")
+        // Update the game state
         enemySelected = false;
+        // Check and see if the game has been won and update the game state accordingly.
         if (enemies.length == 0) {
             gameWon = true;
         }
-    } else {
-        $("#defenderStatus").text(currentDefender.name + " counterattacked " + userCharacter.name + " for " + currentDefender.counterPwr + " damage.");
+    }
+    // Otherwise, if the enemy is still alive...
+    else {
+        // The enemy counter attacks
         userCharacter.health -= currentDefender.counterPwr;
-        userCharacter.currentAttPwr += userCharacter.attPwr;
+        // Write the enemy's counterattack to the DOM
+        $("#defenderStatus").text(currentDefender.name + " counterattacked " + userCharacter.name + " for " + currentDefender.counterPwr + " damage.");
+        // Check and see if the player is dead and update the game state accordingly.
         if (userCharacter.health <= 0) {
             gameOver = true;
         }
@@ -108,27 +147,37 @@ function attack() {
 }
 
 
+// Main function to update the overall state of the game.
 function updateGameStatus() {
-    if(!gameStarted || !enemySelected || gameWon || gameOver) {
+    // Disable the attack button when it shouldn't be used
+    if (!gameStarted || !enemySelected || gameWon || gameOver) {
         $("#attBtn").attr("disabled", "disabled");
     }
-    
+
+    // Move all characters to the selection row and return if the game hasn't yet started
     if (!gameStarted) {
         moveAllToSelect();
         return;
     }
 
+    // Update the instructions if the game has begun but an enemy hasn't been selected
     if (!enemySelected) {
         $("#instructions").text("Select an enemy to attack.")
-    } else {
+    }
+    // If an enemy has been selected...
+    else {
+        // Enable the attack button
         $("#attBtn").removeAttr("disabled");
+        // Update the instructions
         $("#instructions").text("Press the button to attack!")
+        // Refresh the status of the currently selected characters
+        userIndex = parseInt(userCharacter.value);
+        moveToUser(userIndex);
         defenderIndex = parseInt(currentDefender.value);
-        attackerIndex = parseInt(userCharacter.value);
-        moveToUser(attackerIndex);
         moveToDefense(defenderIndex);
     }
 
+    // If the game is won, update the instructions and cue the music.
     if (gameWon) {
         $("#instructions").text("You win!")
         battleSong.pause();
@@ -136,6 +185,7 @@ function updateGameStatus() {
         gameWonSong.play();
     }
 
+    // If the game is lost, update the instructions and cue the sad music.
     if (gameOver) {
         $("#instructions").text("Game over!")
         battleSong.pause();
@@ -144,41 +194,55 @@ function updateGameStatus() {
     }
 }
 
+// Main function to move a character where they belong.
 function moveCharacter(value) {
+    // If the game has not yet been marked as started...
     if (!gameStarted) {
+        // Move the selected character to the #userRow and update the userCharacter variable.
         moveToUser(value);
         userCharacter = characters[value];
+
+        // Populate the enemies array and send those characters to the #enemyRow
         for (var i in characters) {
             if (i != value) {
                 enemies.push(characters[i]);
             }
         }
         populateEnemyPool(value);
+
+        // Clear the #selectRow
         $("#selectRow").empty();
+
+        // Update the game status and cue the battle music!
         gameStarted = true;
         battleSong.play();
-    } else if (!enemySelected) {
+    }
+    // Otherwise, if the game is in progress but an enemy hasn't been selected...
+    else if (!enemySelected) {
+        // Make sure the user didn't click their own character...
         if (value != userCharacter.value) {
-            enemySelected = true;
+            // Move the character with index value to the #defenseRow
             moveToDefense(value);
+            // Remove that same character from the enemyPool and refresh
             removeFromEnemyPool(value);
             populateEnemyPool();
+            // Update the currentDefender variable and the game state
             currentDefender = characters[value];
+            enemySelected = true;
         }
     }
 }
 
-
-
-
-$(document).ready( function () {
+$(document).ready(function () {
     updateGameStatus();
 
     // Run this code when the user clicks a character
     $(document).on("click", ".character-tile", function () {
+        // Get the clicked character's index value
         var valueString = $(this).attr('value');
         var value = parseInt(valueString);
 
+        // Move that character appropriately and update the game's status
         moveCharacter(value);
         updateGameStatus();
     })
